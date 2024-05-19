01f558907b2dc43d6d58c9bd6105db8a78df2f99
bchathoth-wt <90780158+bchathoth-wt@users.noreply.github.com>
2024-05-19 10:42:37 +0100

    Minor changes to unit test cases

--- test/data-platform-ts.test.ts
+++ test/data-platform-ts.test.ts
@@ -5,14 +5,5 @@
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/data-platform-ts-stack.ts
 describe('Infrastructure Test Suite', () => {
-  test('S3 BucketZones Created', () => {
-    //   const app = new cdk.App();
-    //     // WHEN
-    //   const stack = new DataPlatformTs.DataPlatformTsStack(app, 'MyTestStack');
-    //     // THEN
-    //   const template = Template.fromStack(stack);
-    //   template.hasResourceProperties('AWS::SQS::Queue', {
-    //     VisibilityTimeout: 300
-    //   });
-  });
+  test('Placeholder', () => {});
 });
